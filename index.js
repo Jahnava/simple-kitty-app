a94e7defa43dedcf9a038d79b608d8d9c23b0f2d
jahnava <jahnavalmt@gmail.com>
2017-06-18 22:38:43 -0400

    played around with padding and margins of two boxes

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ server.get('/', function(req, res){
   res.sendFile('index.html', {root: __dirname + '/public/html'});
 });
 
+server.get('/', function(req, res){
+  res.sendFile('page2.html', {root: __dirname + '/public/html'});
+});
+
+
 server.listen(port, function(){
   console.log('Now listening on port...', port);
 });
